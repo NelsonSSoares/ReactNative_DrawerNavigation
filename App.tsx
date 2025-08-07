5e56ef11d5ed6aee49c69e5e6240ebeca0692bfc
Nelson <punkpunkada@gmail.com>
2025-08-07 09:12:29 -0300

    bottom tabs nav

--- App.tsx
+++ App.tsx
@@ -5,11 +5,35 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import WelcomeScreen from './screens/WelcomeScreen';
 import UserScreen from './screens/UserScreen';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
+
   return (
+    <NavigationContainer>
+      <BottomTab.Navigator initialRouteName="WelcomeScreen" screenOptions={{
+
+      }
+      } >
+        <BottomTab.Screen name="WelcomeScreen" component={WelcomeScreen} options={{
+          headerStyle: { backgroundColor: "#610020ff" },
+          headerTintColor: "white",
+          tabBarLabel: "Welcome",
+          tabBarIcon: ({ color }) => <Ionicons name="home" size={24} color={color}/>
+        }} />
+
+
+        <BottomTab.Screen name="UserScreen" component={UserScreen} options={{
+          tabBarIcon: ({ color }) => <Ionicons name="person" size={24} color={color} />,
+        }} />
+
+      </BottomTab.Navigator>
+    </NavigationContainer>
+
+  );
+/*   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="WelcomeScreen" screenOptions={{
         drawerActiveBackgroundColor: "#f0e1ff",
@@ -30,8 +54,8 @@ export default function App() {
         }} />
 
       </Drawer.Navigator>
-    </NavigationContainer>
-  );
+    </NavigationContainer> */
+
 }
 
 const styles = StyleSheet.create({
--- screens/UserScreen.tsx
+++ screens/UserScreen.tsx
@@ -1,10 +1,17 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
-export default function UserScreen(){
+export default function UserScreen({route, navigation}: any) {
+
+    function openDrawer() {
+        navigation.toggleDrawer();
+    }
+
     return (
         <View style={styles.rootContainer}>
             <Text>THIS IS THE <Text style={styles.heighlight}>"USER"</Text> SCREEN</Text>
+            <Button title="Open Drawer" onPress={openDrawer} />
         </View>
+        
     );
 }
 
